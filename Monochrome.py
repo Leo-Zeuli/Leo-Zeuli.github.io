def9660232e703542fd334488d40eb854ce95aa2
Leo-Zeuli <90939039+Leo-Zeuli@users.noreply.github.com>
2021-11-12 00:13:12 -0600

    Bored to Death seaosn 2 and 3 stuff

--- Monochrome.py
+++ Monochrome.py
@@ -1,12 +1,13 @@
 from PIL import Image
 
-original = Image.open("_.jpg")
+original = Image.open("marijuana.png")
 monochromed = Image.new("RGBA", original.size, (0, 0, 0, 0))
 
 for i in range(original.size[0]):
     for j in range(original.size[1]):
         pixel = original.getpixel((i,j))
-        if (pixel[0] < (240)) or (pixel[1] < (240)) or (pixel[2] < (240)):
+        #if (pixel[0] < (240)) or (pixel[1] < (240)) or (pixel[2] < (240)):
+        if pixel != (255, 255, 255, 255):
             monochromed.putpixel((i, j), (0, 0, 0, 255))
 
 monochromed.save("monochromed.png")
