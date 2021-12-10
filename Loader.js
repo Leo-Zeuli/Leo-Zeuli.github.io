1d1684ddfe94068fb7136958ed599194c27d1000
Leo-Zeuli <90939039+Leo-Zeuli@users.noreply.github.com>
2021-12-10 16:55:36 -0600

    added masonary layout for art

--- Loader.js
+++ Loader.js
@@ -1,12 +1,12 @@
 var features = [["Drunk Skeletons","ar"],["Humanoid","ar"],["Space Nightmare","ar"],["Sensation","ar"],["Revolution Storyboard","s"],["Books and Movies","a"],["Black Widow","a"],["Bored to Death","r"],["Bored to Death S1","r"],["Texas Chainsaw Massacre","a"],["Blade Runner 2049","a"],["Loki","r"]];
 var features_loaded = new Array(features.length).fill(false);
 
-var synopsis_pointer = 0;
+var pointer = 0;
 const parent_folder_dictionary = {"a":"Features","r":"Features","s":"Narratives","p":"Narratives","ar":"Art","c":"Art"}
 
 function push_pointer(type=[]) {
   function check() {
-    if (synopsis_pointer >= features.length) {
+    if (pointer >= features.length) {
       $(".load-more").attr("hidden",true);
       return true;
     }
@@ -14,17 +14,18 @@ function push_pointer(type=[]) {
   if (check()) {
     return;
   }
-  while (!type.includes(features[synopsis_pointer][1]) && (type.length != 0)) {
-    synopsis_pointer += 1;
-    console.log(features[synopsis_pointer]);
+  while (!type.includes(features[pointer][1]) && (type.length != 0)) {
+    pointer += 1;
+    console.log(features[pointer]);
     if (check()) {
       return;
     }
   }
 }
+
 function load_synopses(number_of_synopses, type=[]) {
-  var end_index = synopsis_pointer+number_of_synopses;
-  synopsis_pointer += number_of_synopses;
+  var end_index = pointer+number_of_synopses;
+  pointer += number_of_synopses;
   for (let i = end_index-number_of_synopses; i < Math.min(end_index,features.length); i++) {
     if (type.includes(features[i][1]) || (type.length == 0)) {
       $.get(parent_folder_dictionary[features[i][1]]+"/"+features[i][0]+"/Synopsis.html", function(data) {
@@ -43,28 +44,30 @@ function load_synopses(number_of_synopses, type=[]) {
       });
     } else {
       end_index += 1;
-      synopsis_pointer += 1;
+      pointer += 1;
     }
   }
   push_pointer(type);
 }
+
 function load_synopsis_wide(type=[]) {
   if (!document.getElementById("wide-synopses").hasChildNodes()) {
-    while (!(type.includes(features[synopsis_pointer][1])) && !(type.length == 0)) {
-      synopsis_pointer += 1;
+    while (!(type.includes(features[pointer][1])) && !(type.length == 0)) {
+      pointer += 1;
     }
     var div = document.createElement('div');
     div.setAttribute("style", "margin: 25px 0 15px 0");
     div.setAttribute("id", "wide");
     document.getElementById("wide-synopses").appendChild(div);
-    $("#wide").load(parent_folder_dictionary[features[synopsis_pointer][1]]+"/"+features[synopsis_pointer][0].replaceAll(" ","%20")+"/Synopsis%20Wide.html");
-    synopsis_pointer += 1;
+    $("#wide").load(parent_folder_dictionary[features[pointer][1]]+"/"+features[pointer][0].replaceAll(" ","%20")+"/Synopsis%20Wide.html");
+    pointer += 1;
   }
   push_pointer(type);
 }
+
 function load_synopses_compact(number_of_synopses, type=[]) {
-  var end_index = synopsis_pointer+number_of_synopses;
-  synopsis_pointer += number_of_synopses;
+  var end_index = pointer+number_of_synopses;
+  pointer += number_of_synopses;
   for (let i = end_index-number_of_synopses; i < Math.min(end_index,features.length); i++) {
     if (type.includes(features[i][1]) || (type.length == 0)) {
       $.get(parent_folder_dictionary[features[i][1]]+"/"+features[i][0]+"/Synopsis%20Compact.html", function(data) {
@@ -84,10 +87,117 @@ function load_synopses_compact(number_of_synopses, type=[]) {
       });
     } else {
       end_index += 1;
-      synopsis_pointer += 1;
+      pointer += 1;
     }
   }
   push_pointer(type);
+}
+
+function load_art(per_row,number_of_rows, pieces=[]) {
+  var piece_synopses = Array(number_of_rows).fill(undefined).map(e => Array(per_row).fill(undefined));
+  var pieces_to_load = per_row*number_of_rows;
+  var in_progress = 0;
+  var isRunning;
+
+  function load_pieces(pieces_to_load) {
+    var compact_container = document.createElement("div");
+    compact_container.setAttribute("class", "art-flexbox-container");
+    var ratio_sum = 0;
+    for (img of pieces_to_load) {
+      ratio_sum += img[1]/img[2];
+    }
+    var height = parseInt((800-10*(per_row-1))/ratio_sum);
+    for (piece of pieces_to_load) {
+      piece[0].firstElementChild.firstElementChild.setAttribute("style","max-height:"+String(height)+"px");
+      compact_container.appendChild(piece[0]);
+    }
+    document.getElementById("collection-synopses").appendChild(compact_container);
+  }
+
+  function find_undefined() {
+    for (var i = 0; i < piece_synopses.length; i++){
+      for (var j = 0; j < piece_synopses[i].length; j++) {
+        if (piece_synopses[i][j] == undefined){
+          return([i,j]);
+        }
+      }
+    }
+  }
+
+  function load_remaining() {
+    if (pointer >= features.length) {
+      pieces_to_load = 0;
+      for (row of piece_synopses) {
+        filtered_row = row.filter(function(element){return (element != undefined)});
+        if (filtered_row.length != 0) {
+          load_pieces(filtered_row);
+        }
+      }
+    }
+  }
+
+  function load_piece(source) {
+    pieces_to_load -= 1;
+    $.get("/Art/"+source+"/Linked%20Piece.html", function(data) {
+      in_progress += 1;
+      var div = document.createElement("div");
+      div.innerHTML = data;
+      var img = new Image();
+      img.onload = function() {
+        index = find_undefined();
+        piece_synopses[index[0]][index[1]] = [div,this.naturalWidth,this.naturalHeight];
+        for (var row = 0; row < piece_synopses.length; row++) {
+          if (!piece_synopses[row].includes(undefined)) {
+            load_pieces(piece_synopses[row]);
+            piece_synopses[row] = [];
+          }
+        }
+        in_progress -= 1;
+        if ((in_progress <= 0) && (!isRunning)){
+          load_remaining();
+        }
+      };
+      img.src = div.firstElementChild.firstElementChild.getAttribute('src');
+    }).fail(function() {
+      in_progress -= 1;
+      pieces_to_load += 1; 
+      if (!isRunning && (pieces.length != 0)) {fill_piece_synopses()};
+    });
+  }
+
+  if (pieces.length != 0) {
+    isRunning = true;
+    pointer = features.length;
+    for (piece of pieces) {
+      if (pieces_to_load != 0){
+        load_piece(piece);
+      }
+    }
+    isRunning = false;
+    load_remaining();
+  }
+
+  else {
+    function fill_piece_synopses() {
+      isRunning = true;
+
+      if (pointer >= features.length) {
+        load_remaining();
+      }
+
+      while (pieces_to_load != 0) {
+        if (features[pointer][1] == "ar"){
+          load_piece(features[pointer][0]);
+        }
+        pointer += 1;
+        if (pointer >= features.length) {
+          load_remaining();
+        }
+      };
+      isRunning = false;
+    }
+    fill_piece_synopses()
+  }
 }
 
 function load_more(source) {
@@ -107,6 +217,6 @@ function load_more(source) {
   } else if (source == "narratives") {
     load_synopses(6,["s","p"]);
   } else if (source == "art") {
-    load_synopses_compact(6,["ar"]);
+    load_art(3,2);
   }
 };
