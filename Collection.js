95b060f813f70aa5387526d63501f3217dc8cb3d
Leo-Zeuli <90939039+Leo-Zeuli@users.noreply.github.com>
2021-12-11 17:03:48 -0600

    Update Collection.js

--- Collection.js
+++ Collection.js
@@ -7,7 +7,7 @@ function set_pieces(pieces_list) {
 
 function change_piece(direction) {
   pieces_index = (pieces_index+{"right":1,"left":-1}[direction]+pieces.length) % pieces.length;
-  $.get("Art/"+pieces[pieces_index]+"/Linked%20Piece.html", function(data) {
+  $.get("/Art/"+pieces[pieces_index]+"/Linked%20Piece.html", function(data) {
     var piece = document.createElement('div');
     piece.innerHTML = data.trim();
     piece.firstElementChild.firstElementChild.setAttribute("class", "collection-image");
