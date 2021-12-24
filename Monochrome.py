23c9ffbdb2f0e156aeaaf756240a0dd3315ecd95
Leo-Zeuli <90939039+Leo-Zeuli@users.noreply.github.com>
2021-12-24 17:13:20 -0600

    Update Monochrome.py

--- Monochrome.py
+++ Monochrome.py
@@ -1,15 +1,12 @@
 from PIL import Image
 
-original = Image.open("Watchmen-smiley.svg.png")
+original = Image.open("original.jpg")
 monochromed = Image.new("RGBA", original.size, (0, 0, 0, 255))
-
-#print(original.getpixel((500,100)))
 
 for i in range(original.size[0]):
     for j in range(original.size[1]):
         pixel = original.getpixel((i,j))
-        if ((pixel == (0,0,0,0)) or (pixel[1:3] == (31, 37))):
-        #if pixel != (0,0,0):
+        if pixel != (0,0,0):
             monochromed.putpixel((i, j), (0, 0, 0, 0))
 
 monochromed.save("monochromed.png")
