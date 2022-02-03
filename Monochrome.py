c1a7a0fe355dc2960167122fa1c78626b4bb1e38
Leo-Zeuli <90939039+Leo-Zeuli@users.noreply.github.com>
2022-02-03 14:21:39 -0600

    Succession

--- Monochrome.py
+++ Monochrome.py
@@ -1,12 +1,12 @@
 from PIL import Image
 
-original = Image.open("original.jpg")
+original = Image.open("original.png")
 monochromed = Image.new("RGBA", original.size, (0, 0, 0, 255))
 
 for i in range(original.size[0]):
     for j in range(original.size[1]):
         pixel = original.getpixel((i,j))
-        if pixel != (0,0,0):
+        if pixel == (255,255,255,0):
             monochromed.putpixel((i, j), (0, 0, 0, 0))
 
 monochromed.save("monochromed.png")
